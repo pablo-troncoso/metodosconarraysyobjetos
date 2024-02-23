1842b9a77ae69b5014b7efb55b7a60378efb44ca
pablotroncoso <pablotroncoso@gmail.com>
2024-02-23 15:20:18 -0300

    Actualización

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,53 @@ const dental = [
 radiologia.shift(); 
 radiologia.pop();   
 
+// Ejercicio 3 Imprimir en la página HTML con document.write
+document.write('<h2>Consultas Médicas Dental:</h2>'); 
+for (let i = 0; i < dental.length; i++) {
+    let consulta = dental[i];
+    document.write(`<p>${consulta.hora} - ${consulta.especialista} - ${consulta.paciente} - ${consulta.rut} - ${consulta.prevision}</p>`);
+}
+
+// Ejercicio 4 Imprimir un listado total de todos los pacientes que se atendieron en el centro médico. 
+let pacientesTotales = [];
+
+pacientesTotales = pacientesTotales.concat(radiologia.map(consulta => consulta.paciente));
+pacientesTotales = pacientesTotales.concat(traumatologia.map(consulta => consulta.paciente));
+pacientesTotales = pacientesTotales.concat(dental.map(consulta => consulta.paciente));
+
+document.write('<h2>Lista de todos los Pacientes Atendidos:</h2>');
+pacientesTotales.forEach(paciente => {
+    document.write(`<p>${paciente}</p>`);
+});
+
+//Ejercicio 5 Pacientes que indican ser de ISAPRE en la lista de consultas médicas de Dental
+document.write('<h2>Pacientes de ISAPRE en la lista de Dental</h2>');
+
+const pacientesIsapreDental = dental.filter(consulta => consulta.prevision === 'ISAPRE');
+pacientesIsapreDental.forEach(consulta => {
+    document.write(`<p>${consulta.paciente} - ${consulta.prevision}</p>`);
+});
+
+// Ejercicio 6 Filtrar Pacientes de FONASA en la lista de Traumatología
+document.write('<h2>Pacientes de FONASA en la lista de Traumatología</h2>');
+
+const pacientesFonasaTraumatologia = traumatologia.filter(consulta => consulta.prevision === 'FONASA');
+pacientesFonasaTraumatologia.forEach(consulta => {
+    document.write(`<p>${consulta.paciente} - ${consulta.prevision}</p>`);
+});
+
+
+
+let pacientesRadiologia = obtenerListaPacientes(radiologia);
+let pacientesTraumatologia = obtenerListaPacientes(traumatologia);
+let pacientesDental = obtenerListaPacientes(dental);
+
+let pacientesTotal = pacientesRadiologia.concat(pacientesTraumatologia, pacientesDental);
+
+document.write('<h2>Listado de Pacientes Atendidos:</h2>'); 
+pacientesTotal.forEach(paciente => {
+    document.write(`<p>${paciente}</p>`);
+});
 
 document.write(`<p>Cantidad de atenciones para Radiología: ${radiologia.length}</p>`);
 document.write(`<p>Cantidad de atenciones para Traumatología: ${traumatologia.length}</p>`);
@@ -48,3 +95,6 @@ document.write(`<p>Cantidad de atenciones para Dental: ${dental.length}</p>`);
 document.write(`<p>Primera atención Radiología: ${radiologia[0].paciente} - ${radiologia[0].prevision} | Última atención Radiología: ${radiologia[radiologia.length -1].paciente} - ${radiologia[radiologia.length -1].prevision}.</p>`);
 document.write(`<p>Primera atención Traumatología: ${traumatologia[0].paciente} - ${traumatologia[0].prevision} | Última atención Traumatología: ${traumatologia[traumatologia.length -1].paciente} - ${traumatologia[traumatologia.length -1].prevision}.</p>`);
 document.write(`<p>Primera atención Dental: ${dental[0].paciente} - ${dental[0].prevision} | Última atención Dental: ${dental[dental.length -1].paciente} - ${dental[dental.length -1].prevision}.</p>`);
+
+
+
